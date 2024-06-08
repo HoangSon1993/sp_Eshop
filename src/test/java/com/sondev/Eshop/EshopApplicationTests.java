4da574506de4b9c4b40d60d5c95ad0eaad107a57
Hoang Son <son.lyhoang2014@gmail.com>
2024-06-08 09:51:17 +0700

    Initial project with Spring Initializr

---
+++ src/test/java/com/sondev/Eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sondev.Eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
